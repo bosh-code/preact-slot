3b95654b24547da7c3999f6cb1d9c09915d2b175
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-10-20 13:04:44 +1300

    feat(coverage): add test:coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,30 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [preactPlugin()],
   test: {
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './coverage',
+      thresholds: {
+        perFile: true,
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80
+      },
+      reporter: [
+        'text',
+        'json-summary',
+        'html-spa',
+        'lcov'
+      ],
+      all: true,
+      include: [
+        'src/**/*.tsx'
+      ],
+      exclude: [
+        'src/**/index.ts'
+      ]
+    },
     environment: 'happy-dom',
     globals: true,
     setupFiles: './test-setup.ts',
